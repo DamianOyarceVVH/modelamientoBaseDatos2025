157f09d25ad4904c31e0c1e6c2897ba4fd5118c5
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-05-20 10:40:42 -0400

    2.2.2

--- 1.0_Recordando_SQL/2.2_Insercion_datos.sql
+++ 1.0_Recordando_SQL/2.2_Insercion_datos.sql
@@ -1,20 +1,34 @@
+use mercado;
+
 insert into tipos_usuarios (nombre_tipo, descripcion_tipo, created_by, updated_by)
 values ('Administrador', 'Accede a todas las funciones del sistema, incluida la administración de usuarios.', 1, 1),
 ('Vendedor', 'Interactúa con los clientes para vender los productos', 1, 1);
 
-insert into usuario (nombre_usuario, password, correo, tipo_usuario_id, created_by, updated_by)
+insert into usuarios (nombre_usuario, password, correo, tipo_usuario_id, created_by, updated_by)
 values ('sistema', 'bcrypt', 'sistema@plataforma.cl', null, null, null),
 ('javier.mendez', '·$&2347$·', 'javiermendez@gmail.com', 1, 1, 1),
 ('max.reyes', '$t3ph3nCurry', 'maxreyes@gmail.com', 2, 1, 1),
 ('benjamin.gajardo', 'L4m1n3Y4m4L', 'benjardou@gmail.com', 2, 1, 1);
 
 insert into productos (nombre_producto, precio, stock, created_by, updated_by)
-values ('Coca-cola', 1200, 500, 1, 1),
-('Mini-cockies', 400, 200, 1, 1),
-('Doritos', 600, 300, 1, 1);
+values ('Coca-cola', 850000, 500, 1, 1),
+('Mini-cockies', 60000, 200, 1, 1),
+('Doritos', 180000, 300, 1, 1);
 
 insert into ventas (usuario_id, fecha, created_by, updated_by)
-values (2, 23-01-2025, 1, 1),
-(3, 17-02-2025, 1, 1),
-(3, 12-03-2025, 1, 1),
-(2, 30-04-2025, 1, 1);
+values (2, '2025-01-23', 1, 1),
+(3, '2025-02-17', 1, 1),
+(3, '2025-03-12', 1, 1),
+(2, '2025-04-30', 1, 1);
+
+insert into detalles_ventas (venta_id, producto_id, cantidad, precio_unitario, created_by, updated_by)
+values (2, 3, 50, 600, 1, 1),
+(4, 2, 42, 400, 1, 1),
+(3, 1, 70, 1700, 1, 1),
+(1, 1, 100, 1700, 1, 1);
+
+select*from tipos_usuarios;
+select*from usuarios;
+select*from productos;
+select*from ventas;
+select*from detalles_ventas;

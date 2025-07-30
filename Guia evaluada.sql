347b698fd9ca2907df3a73d53830c9411db67d31
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-07-30 09:31:09 -0400

    parte 2

--- Guia evaluada.sql
+++ Guia evaluada.sql
@@ -30,3 +30,27 @@ set correo = 'andresitolopez@gmail.com' where id_cliente = 3;
 
 update reservas
 set fecha_ingreso = '2025-08-08' where id_reserva = 2;
+
+-- =========================================
+-- Parte 2: Borrado Lógico y Filtrado Activo
+-- =========================================
+
+-- Borrado lógico
+update habitaciones 
+set deleted = 1 where id_habitacion = 3;
+
+update clientes 
+set deleted = 1 where id_cliente = 5;
+
+select * from habitaciones
+where deleted = 0;
+
+select * from clientes
+where deleted = 0;
+
+-- Listar registros inactivos
+select * from habitaciones
+where deleted = 1;
+
+select * from clientes
+where deleted = 1;
